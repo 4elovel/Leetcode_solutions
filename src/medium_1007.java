15025c800908189814eae84bd88b514afeb28486
4elovel <109417314+4elovel@users.noreply.github.com>
2025-05-03 18:03:06 +0000

    refactored medium_1007 implementation

--- src/medium_1007.java
+++ src/medium_1007.java
@@ -1,32 +1,21 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        int count1=0;
-        int count2=0;
-        for(int i=1;i<tops.length;i++){
-            boolean state1 = tops[0] != tops[i] && tops[0] != bottoms[i];//counter1 is illegal
-            boolean state2 = bottoms[0] != tops[i] && bottoms[0] != bottoms[i];//counter2 is illegal
-            if(state1) count1=20001;
-            if(state2) count2=20001;
-            if(bottoms[i]==tops[0] && tops[i]==bottoms[0] && tops[i]!=bottoms[i]){
-                count1++;
-                count2++;
-                continue;
-            }
-            if(bottoms[i]==tops[0]&&tops[i]!=tops[0]&& tops[i]!=bottoms[i]) {
-                count1++;
-                continue;
-            }
-            System.out.println("i: "+i+  " tops[i]: "+tops[i]+" bottoms[0]: "+bottoms[0]+" bottoms[i]: "+bottoms[i]+" bottoms[0]: "+bottoms[0]);
-            System.out.println("cnt1: "+count1+ " cnt2: "+count2);
-            if(tops[i]==bottoms[0]&&bottoms[i]!=bottoms[0]&& tops[i]!=bottoms[i]) {
-                count2++;
-                continue;
-            }
-
-    
+        int rotations1 = check(tops[0], tops, bottoms);
+        int rotations2 = check(bottoms[0], tops, bottoms);
+        //System.out.println(rotations1+" "+rotations2);
+        int minRot = rotations1<rotations2?rotations1:rotations2;
+        return (minRot == Integer.MAX_VALUE) ? -1 : minRot;
+    }
+    private int check(int target, int[] tops, int[] bottoms) {
+        int rotTop=0;
+        int rotBot=0;
+        for(int i = 0;i<tops.length;i++){
+            //System.out.println(target+" "+bottoms[i]+" "+ tops[i]);
+            //System.out.println((target!=bottoms[i])&&(target!=tops[i]));
+            if(target != bottoms[i]&& target!=tops[i]) return Integer.MAX_VALUE;
+            if(target != bottoms[i]) rotBot++;
+            if(target != tops[i]) rotTop++;
         }
-        System.out.println("cnt1: "+count1+ " cnt2: "+count2);
-        if(count1>20000&&count2>20000) return -1;
-        return count1>count2?count2:count1;
-    }     
+        return rotTop<rotBot?rotTop:rotBot;
+    }
 }
